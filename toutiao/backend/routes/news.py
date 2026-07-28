63355c4abdd89a204f70564fb523cbb9433faa7e
王德康 <3177146160@qq.com>
2026-07-28 17:35:06 +0800

    feat: 获取新闻分类-模块化路由

--- toutiao/backend/routes/news.py
+++ toutiao/backend/routes/news.py
@@ -7,6 +7,17 @@ router = APIRouter(prefix="/api/news", tags=["news"])
 
 
 @router.get("/categories")
-async def get_categories():
-    return {"message": "获取分类成功"}
- 
+async def get_categories(skip: int = 0, limit: int = 100):
+    return {"code": 200, "message": "获取新闻分类成功", "data": "新闻分类列表"}
+
+
+
+
+
+
+
+# 接口实现流程
+# 1 模块化路由 -> API接口规范文档
+# 2 定义模型类 -> 数据库表(数据库设计文档)
+# 3 在 crud 文件夹里面创建文件，封装操作数据库的方法
+# 4 在路由处理函数里面调用 crud 封装好的方法，响应结果
