025cb363cad21a87cddf497c0a797ffefc92252f
王德康 <3177146160@qq.com>
2026-07-28 18:47:14 +0800

    feat: cors注释

--- toutiao/backend/main.py
+++ toutiao/backend/main.py
@@ -4,12 +4,20 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+# 实际企业级生产环境一般配置nginx解决跨域
+origins =[
+    'http://localhost',
+    "http://localhost:3000",
+    "https://your-fronted-domain.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
+    allow_origins=["*"],    # 允许的源,开发阶段允许所有源,生产环境需要指定源
+    allow_credentials=True, # 允许携带cookie
+    allow_methods=['*'],    # 允许的请求方法
+    allow_headers=['*'],    #允许的请求头
 )
 
 
