8a6b3133efc5bb125ae645fc1eb09493d42be62d
王德康 <3177146160@qq.com>
2026-07-30 10:30:14 +0800

    feat: 配置Redis客户端

---
+++ toutiao/backend/config/cache_config.py
@@ -0,0 +1,14 @@
+import redis.asyncio as redis
+
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+REDIS_DB = 0
+
+
+# 创建 Redis 的连接对象
+redis_client = redis.Redis(
+    host=REDIS_HOST,  # Redis 服务器的主机地址
+    port=REDIS_PORT,  # Redis 端口号
+    db=REDIS_DB,  # Redis 数据库编号，0~15
+    decode_responses=True  # 是否将字节数据解码为字符串
+)
