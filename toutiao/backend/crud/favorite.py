80f82d5aca595a56dd2198ec6fb6c30d23e649ea
王德康 <3177146160@qq.com>
2026-07-30 08:35:02 +0800

    feat: 获取收藏列表

--- toutiao/backend/crud/favorite.py
+++ toutiao/backend/crud/favorite.py
@@ -1,7 +1,8 @@
-from sqlalchemy import select,delete
+from sqlalchemy import select, delete, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.favorite import Favorite
+from models.news import News
 
 
 # 检查收藏状态：当前用户 是否 收藏了这一条新闻
@@ -52,3 +53,34 @@ async def remove_news_favorite(
 
     await db.commit()
     return result.rowcount > 0
+
+
+
+# 获取收藏列表：获取的是某个用户的收藏列表 + 分页功能
+async def get_favorite_list(
+        db: AsyncSession,
+        user_id: int,
+        page: int = 1,
+        page_size: int = 10
+):
+    # 总量 + 收藏的新闻列表
+    count_query = select(func.count()).where(Favorite.user_id == user_id)
+    count_result = await db.execute(count_query)
+    total = count_result.scalar_one()
+
+    # 获取收藏列表 - 联表查询 join() + 收藏时间排序 + 分页
+    # select(查询主体模型类, 字段别名).join(联合查询的模型类, 联合查询的条件).where().order_by().offset().limit()
+    # 别名： Favorite.created_at.label("favorite_time")
+    offset = (page - 1) * page_size
+    # [
+    #   (新闻对象, 收藏时间, 收藏id)
+    # ]
+    query = (select(News, Favorite.created_at.label("favorite_time"), Favorite.id.label("favorite_id"))
+             .join(Favorite, Favorite.news_id == News.id)
+             .where(Favorite.user_id == user_id)
+             .order_by(Favorite.created_at.desc())
+             .offset(offset).limit(page_size)
+             )
+    result = await db.execute(query)
+    rows = result.all()
+    return rows, total
--- toutiao/backend/routes/favorite.py
+++ toutiao/backend/routes/favorite.py
@@ -6,11 +6,11 @@ from models.users import User
 from utils.auth import get_current_user
 from utils.response import success_response
 from crud import favorite
-from schemas.favorite import FavoriteCheckResponse,FavoriteAddRequest
+from schemas.favorite import FavoriteCheckResponse,FavoriteAddRequest,FavoriteListResponse
 
 router = APIRouter(prefix="/api/favorite", tags=["favorite"])
 
-
+# 检查收藏状态
 @router.get("/check")
 async def check_favorite(
     news_id: int = Query(default=..., alias="newsID"),
@@ -49,3 +49,23 @@ async def remove_favorite(
         )
 
     return success_response(message="删除收藏成功")
+
+
+# 获取收藏列表
+@router.get("/list")
+async def get_favorite_list(
+        page: int = Query(1, ge=1),
+        page_size: int = Query(10, ge=1, le=100, alias="pageSize"),
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+):
+    rows, total = await favorite.get_favorite_list(db, user.id, page, page_size)
+    favorite_list = [{
+        **news.__dict__,
+        "favorite_time": favorite_time,
+        "favorite_id": favorite_id
+    } for news, favorite_time, favorite_id in rows]
+    has_more = total > page * page_size
+
+    data = FavoriteListResponse(list=favorite_list, total=total, hasMore=has_more)
+    return success_response(message="获取收藏列表成功", data=data)
